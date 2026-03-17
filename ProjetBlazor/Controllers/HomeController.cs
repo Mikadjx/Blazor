b250a1eed6d3a2f9fb2c9000d298106e7c45c6c8
mickael <chezdijoux@gmail.com>
2026-03-17 22:18:36 +0100

    feat: add homepage with dynamic products

---
+++ ProjetBlazor/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetPME.Data;
+
+namespace AspNetPME.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly AppDbContext _db;
+
+    public HomeController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _db.Products.ToList();
+        return View(products);
+    }
+}
--- ProjetBlazor/Program.cs
+++ ProjetBlazor/Program.cs
@@ -1,10 +1,22 @@
+
 using ProjetBlazor.Components;
+using Microsoft.EntityFrameworkCore;
+using ProjetBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    )
+);
 
 var app = builder.Build();
 
